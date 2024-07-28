5df30ba6bde567a763147dd8598bc58a3e5ca595
Joe <jo3.l.dev@outlook.com>
2024-07-27 21:57:24 -0700

    highlight: simplify script with glob pattern

--- scripts/highlight.mjs
+++ scripts/highlight.mjs
@@ -3,10 +3,12 @@ import { createHighlighter, bundledLanguages } from 'shiki';
 
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
+
+import { glob } from 'glob';
 
 import { parseDocument } from 'htmlparser2';
-import { findAll, textContent, replaceElement } from 'domutils';
+import { findAll, findOne, textContent, replaceElement } from 'domutils';
 import render from 'dom-serializer';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -32,7 +34,7 @@ async function main() {
 	const nordDarkerTheme = await readJsonFile(join(__dirname, 'nord-darker.json'));
 	await highlighter.loadTheme(nordDarkerTheme);
 
-	const files = await listBuiltIndexHtmlFiles();
+	const files = await glob(join(__dirname, '../public/{docs,learn}/**/index.html'));
 	await Promise.all(
 		files.map((filepath) => highlightFile(highlighter, filepath, { lightTheme: LIGHT_THEME, darkTheme: DARK_THEME })),
 	);
@@ -44,45 +46,25 @@ async function readJsonFile(filepath) {
 	return JSON.parse(await readFile(filepath, { encoding: 'utf-8' }));
 }
 
-// Return the paths of all index.html files under the public/docs and public/learn directories.
-async function listBuiltIndexHtmlFiles() {
-	const publicDir = join(__dirname, '../public');
-
-	const docsDir = join(publicDir, 'docs');
-	const docsIndexFiles = (await readdir(docsDir, { recursive: true, encoding: 'utf-8' }))
-		.filter((filename) => filename.endsWith('index.html'))
-		.map((filename) => join(docsDir, filename));
-
-	const learnDir = join(publicDir, 'learn');
-	const learnIndexFiles = (await readdir(learnDir, { recursive: true, encoding: 'utf-8' }))
-		.filter((filename) => filename.endsWith('index.html'))
-		.map((filename) => join(learnDir, filename));
-
-	return [...docsIndexFiles, ...learnIndexFiles];
-}
-
 async function highlightFile(highlighter, filepath, { lightTheme, darkTheme }) {
 	const contents = await readFile(filepath, { encoding: 'utf-8' });
-	await writeFile(filepath, highlightHtml(highlighter, contents, { lightTheme, darkTheme }));
+	await writeFile(filepath, highlightHtmlContent(highlighter, contents, { lightTheme, darkTheme }));
 }
 
-const codeLanguageRe = /<code class="language-(\w+)">/;
-
-function highlightHtml(highlighter, htmlContent, { lightTheme, darkTheme }) {
+function highlightHtmlContent(highlighter, htmlContent, { lightTheme, darkTheme }) {
 	const doc = parseDocument(htmlContent);
 	for (const preNode of findAll((e) => e.name === 'pre', doc.children)) {
-		const codeNode = preNode.childNodes[0];
+		const codeNode = findOne((e) => e.name === 'code', preNode.children);
 		if (!codeNode) continue;
 
-		const lang = codeLanguageRe.exec(render(codeNode))?.[1] ?? 'text';
-
+		const lang = codeNode.attribs['class']?.replace(/^language-/, '') ?? 'text';
 		const code = textContent(codeNode);
 		const highlighted = highlighter.codeToHtml(code, {
 			lang,
 			themes: { light: lightTheme, dark: darkTheme },
 		});
 
-		const highlightedPreNode = parseDocument(highlighted).childNodes[0];
+		const highlightedPreNode = parseDocument(highlighted).children[0];
 		replaceElement(preNode, highlightedPreNode);
 	}
 

6a59d5a97cbabd3ac514817cb598a1aa58d1526e
Luca Zeuch <l-zeuch@email.de>
2025-01-01 11:43:59 +0100

    frontend: add selfhosting view toggle
    Preliminary commit to cater some feedback about its design and
    underlying JS code; will implement actual content modification later.
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,22 @@
 // Put your custom JS code here
+
+// self-hosting view.
+const getSelfhost = () => {
+  return localStorage.getItem('selfhost') || 'no';
+};
+
+const setSelfhost = (selfhost) => {
+  localStorage.setItem('selfhost', selfhost);
+  document.documentElement.setAttribute('data-selfhost', selfhost);
+};
+
+setSelfhost(getSelfhost());
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('[data-selfhost-value]').forEach((element) => {
+    element.addEventListener('click', (event) => {
+      const selfhost = element.getAttribute('data-selfhost-value');
+      setSelfhost(selfhost);
+    });
+  }
+)});
